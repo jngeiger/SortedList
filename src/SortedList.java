2fab7f5585d2203698a8a7b8b6a26b80b7780dd4
jan <jg@geiger-digital.de>
2020-07-12 18:28:24 +0200

    Initial commit

---
+++ src/SortedList.java
@@ -0,0 +1,62 @@
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class SortedList<T extends Comparable<T>> {
+    class Node{
+        T payload;
+        Node next;
+        Node(T value)
+        {
+            this.payload = value;
+        }
+    }
+    Node head;
+
+    public void insert(T value, BiFunction<T,T, Boolean> func){
+
+        if (head == null)
+        {
+            head = new Node(value);
+        }
+        else {
+            if (func.apply(value,head.payload))
+            {
+                Node temp = head;
+                head = new Node(value);
+                head.next = temp;
+            }
+
+            else {
+                Node currentNode = head;
+                while (currentNode.next != null)
+                {
+                    if (func.apply(value,currentNode.next.payload))
+                    {
+                        Node temp = currentNode.next;
+                        currentNode.next = new Node(value);
+                        currentNode.next.next = temp;
+                        return;
+                    }
+                    currentNode = currentNode.next;
+                }
+                currentNode.next = new Node(value);
+            }
+        }
+    }
+
+    public String toString()
+    {
+        String retVal = "";
+        Node currentNode = head;
+        while (currentNode != null)
+        {
+            retVal += currentNode.payload;
+            if (currentNode.next != null)
+                retVal += ",";
+            currentNode = currentNode.next;
+        }
+        return retVal;
+    }
+    public static void main(String[] args)
+    { }
+}
